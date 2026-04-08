fd0de98a4530bb999806ef26b1ec3b1bbdb1c280
JaideepVerma <61654942+JaideepVerma@users.noreply.github.com>
2026-04-08 21:41:05 +0530

    Update mainRunner.py

--- mainRunner.py
+++ mainRunner.py
@@ -5,9 +5,13 @@ import ZS
 
 def run_all():
     AMZ.main()
+    print('----- AMZ Done -----')
     jpmc.main()
+    print('----- JPMC Done -----')
     swiggy.main()
+    print('----- Swiggy Done -----')
     ZS.main()
+    print('----- ZS Done -----')
 
 if __name__ == "__main__":
     run_all()
