f56fe350993cf6a906d1a3ca9da2c2d2db1d7158
JaideepVerma <61654942+JaideepVerma@users.noreply.github.com>
2026-04-09 22:24:12 +0530

    Update mainRunner.py
    added barclays

--- mainRunner.py
+++ mainRunner.py
@@ -2,6 +2,7 @@ import AMZ
 import jpmc
 import swiggy
 import ZS
+import barclays
 
 def run_all():
     AMZ.main()
@@ -12,6 +13,8 @@ def run_all():
     print('----- Swiggy Done -----')
     ZS.main()
     print('----- ZS Done -----')
-
+    barclays.main()
+    print('----- Barclays Done -----')
+    
 if __name__ == "__main__":
     run_all()
