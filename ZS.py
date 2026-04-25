3779a8e9a69741a79778033aa7890e7c71bae5ad
JaideepVerma <61654942+JaideepVerma@users.noreply.github.com>
2026-04-25 12:16:29 +0530

    Update ZS.py

--- ZS.py
+++ ZS.py
@@ -75,7 +75,7 @@ def scrape_zs():
                     "responsibilities": responsibilities1,
                     "qualifications": responsibilities2,
                     "location": location,
-                    "posting_date": posted_date,
+                    "posting_date": get_ist_timestamp(posted_date),
                     "update_date" : 'update_date',
                     "apply_link": apply_url            
                 })
